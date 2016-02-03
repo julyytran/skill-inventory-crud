7aae4c569752826fa9ce9b02a11723fe5b951fea
July Tran <julyytran@gmail.com>
2016-02-02 18:16:09 -0700

    completed crud

--- app/controllers/skill_inventory_app.rb
+++ app/controllers/skill_inventory_app.rb
@@ -1,8 +1,50 @@
+require 'models/skill_inventory'
+
 class SkillInventoryApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
+  set :method_override, true
+
+  get '/' do
+    erb :dashboard
+  end
 
   get '/skills' do
+    @skills = skill_inventory.all
     erb :index
+  end
+
+  get '/skills/new' do
+    erb :new
+  end
+
+  post '/skills' do
+    skill_inventory.create(params[:skill])
+    redirect '/skills'
+  end
+
+  def skill_inventory
+    database = YAML::Store.new('db/skill_inventory')
+    @skill_inventory ||= SkillInventory.new(database)
+  end
+
+  get '/skills/:id' do |id|
+    @skill = skill_inventory.find(id.to_i)
+    erb :show
+ end
+
+  get '/skills/:id/edit' do |id|
+    @skill = skill_inventory.find(id.to_i)
+    erb :edit
+  end
+
+  put '/skills/:id' do |id|
+    skill_inventory.update(id.to_i, params[:skill])
+    redirect "/skills/#{id}"
+  end
+
+  delete '/skills/:id' do |id|
+    skill_inventory.delete(id.to_i)
+    redirect '/skills'
   end
 
 end
